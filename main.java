40aa526fd4ffd22cae6a515d1b7de0341c66c7c0
katayama <tattu.0310@gmail.com>
2022-09-06 19:20:08 +0900

    ✨feat/func

--- main.java
+++ main.java
@@ -1,12 +1,17 @@
+import java.util.Date;
+
 public class Main {
+    public static void hello(String name) {
+        System.out.println(name);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        hello("katayama");
         String s1 = "Hello";
         String s2 = "Hello";
-        if(s1.equals(s2)) {
+        if (s1.equals(s2)) {
             System.out.println("s1 and s2 are equal");
-        }
-        else {
+        } else {
             System.out.println("s1 and s2 are not equal");
         }
         StringBuilder sb = new StringBuilder();
@@ -14,5 +19,9 @@ public class Main {
             sb.append(i);
         }
         System.out.println(sb.toString());
+
+        Date now = new Date();
+        System.out.println(now);
     }
+
 }
