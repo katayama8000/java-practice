b968da8c8de54e4328ad2d205dd4486383555d76
katayama <tattu.0310@gmail.com>
2022-09-18 16:37:18 +0900

    ✨ add/lambda

---
+++ lambda/Main.java
@@ -0,0 +1,14 @@
+package lambda;
+
+public class Main {
+
+    public static int mainSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Sam sam = Main::mainSum;
+        System.out.println("3+5" + sam.sum(3, 5));
+    }
+
+}
---
+++ lambda/Sam.java
@@ -0,0 +1,7 @@
+package lambda;
+
+//抽象メソッドを一つしか含まないインターフェイス
+public interface Sam {
+    // 抽象メソッド
+    public abstract int sum(int a, int b);
+}
