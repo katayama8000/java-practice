8773cdb97aa4a0141fc8dfcf6472a10aab28705e
katayama <tattu.0310@gmail.com>
2022-09-19 16:24:53 +0900

    ✨ feat/data

---
+++ Youtube/Lesson2/chapter3/Main.java
@@ -0,0 +1,16 @@
+package Youtube.Lesson2.chapter3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int month = 12;
+        int day = 31;
+        // fを忘れるとエラーになる
+        float weight = 60.0f;
+        double height = 170.3;
+        char bloodType = 'A';
+
+        String name = "Taro";
+    }
+
+}
