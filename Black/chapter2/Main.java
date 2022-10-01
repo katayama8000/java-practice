1c8ff0c0ed61dd51bd274423ac12db9d5177af8f
katayama <tattu.0310@gmail.com>
2022-10-01 17:59:45 +0900

    ✨ type

--- Black/chapter2/Main.java
+++ Black/chapter2/Main.java
@@ -2,6 +2,9 @@ package Black.chapter2;
 
 public class Main {
     public static void main(String... args) {
-
+        String str = "JavaJavaJava";
+        String str2 = str.replaceAll("Java", "javaScript");
+        System.out.println(str);
+        System.out.println(str2);
     }
 }
--- Black/chapter3/Main.java
+++ Black/chapter3/Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         Sample a = new Sample(1, "a");
+        Sample c = a;
         Sample b = new Sample(1, "b");
         System.out.println(a.equals(b));
+        System.out.println(a == b);
+        System.out.println(a == c);
     }
 
 }
