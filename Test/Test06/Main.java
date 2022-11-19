3d9fbf78e1599d3f82fc467fc41c4c1fd3aec90a
katayama <tattu.0310@gmail.com>
2022-11-19 17:24:47 +0900

    update

--- Test/Test06/Main.java
+++ Test/Test06/Main.java
@@ -9,6 +9,12 @@ public class Main {
 
         Sample3 s3 = new Sample3();
         s3.hello();
+
+        Sample4 s4 = new Sample4();
+        System.out.println(s4.getImportantNumber());
+        s4.setImportantNumber(100);
+        System.out.println(s4.getImportantNumber());
+        
     }
 
     private static void modify(Sample s) {
---
+++ Test/Test06/Sample4.java
@@ -0,0 +1,13 @@
+package Test.Test06;
+
+public class Sample4 {
+    private int importantNumber = 20001212;
+
+    public int getImportantNumber() {
+        return this.importantNumber;
+    }
+
+    public void setImportantNumber(int importantNumber) {
+        this.importantNumber = importantNumber;
+    }
+}
